537e514c159d5e407bd06b8fa65735a63e2df56c
ElizaLU <ELIZA.LUSZCZEWSKA@GMAIL.com>
2019-10-16 19:05:41 +0200

    test

--- team/router.js
+++ team/router.js
@@ -2,15 +2,21 @@ const { Router } = require('express')
 const Team = require('./model')
 
 const router = new Router()
-
+//test: http :4000/team 
 router.get('/team', (req, res, next) => {
   Team.findAll()
     .then(team => { res.json(team) })
     .catch(next)
 });
-
+//test: http :4000/team name=test
 router.post('/team', (req, res, next) => {
   Team.create(req.body)
+    .then(team => res.json(team))
+    .catch(next)
+})
+
+router.get('/team/:id', (res, req, next) => {
+  Team.findByPk(req.params.id)
     .then(team => res.json(team))
     .catch(next)
 })
